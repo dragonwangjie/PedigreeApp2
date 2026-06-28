1702429ad8309d30287aa32f6956e1e63251a7fd
dragonwangjie <50239695+dragonwangjie@users.noreply.github.com>
2026-06-28 09:23:06 +0800

    chore: add kotlin android plugin declaration (1.9.10) to root plugins block to ensure module plugin resolves

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,9 +6,12 @@ buildscript {
     }
     dependencies {
         classpath("com.android.tools.build:gradle:8.2.0")
+        // Optional: keep Kotlin Gradle plugin on the classpath for older plugin resolution
+        // classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
     }
 }
 
 plugins {
     id("com.android.application") version "8.2.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
 }
